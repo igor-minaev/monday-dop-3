22aafbff7b0a6b6c0e80ae4772accf3e60cd7869
Igor Minayeu <igor.minayeu@gmail.com>
2023-10-14 20:40:10 +0300

    add useEffect and useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+type TodosType = {
+    completed: boolean
+    id: number
+    title: string
+    userId: number
+}
+
 function App() {
+
+
+    const [todos, setTodos] = useState<TodosType[]>([])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json => setTodos(json))
+    }, [])
+
+    const todosForRender = todos.map(t => {
+        return (
+            <li>
+                <input type="checkbox" checked={t.completed}/>
+                <span><b>Id:</b> {t.id} </span>
+                <span><b>UserId:</b> {t.userId} </span>
+                <span><b>Title:</b> {t.title}</span>
+            </li>
+        )
+    })
     return (
         <div className="App">
-
+            <ul>
+                {todosForRender}
+            </ul>
         </div>
     );
 }
